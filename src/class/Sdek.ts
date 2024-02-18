0d9eb14c4140c88ddc6ecfa122455dbca5ed74e5
Aboltys754 <warwar754@mail.ru>
2024-02-18 22:39:47 +0500

    create table

--- src/class/Sdek.ts
+++ src/class/Sdek.ts
@@ -1,4 +1,11 @@
 import Bot from './Bot';
+import * as db from '../libs/db';
+
+
+const urlGetToken = 'https://api.edu.cdek.ru/v2/oauth/token?parameters'
+const grantType = 'client_credentials'
+const clientId = 'EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI'
+const clientSecret = 'PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG'
 
 
 export default class Sdek extends Bot {
@@ -10,6 +17,62 @@ export default class Sdek extends Bot {
     scope: string | null = null
     jti: string | null = null
 
+    get_access_token() {
+        return this.access_token
+    }
+
+    get_jti() {
+        return this.jti
+    }
+
+    async set_access_json() {
+        const tokens = await fetch(`${urlGetToken}&grant_type=${grantType}&client_id=${clientId}&client_secret=${clientSecret}`, {
+            method: 'POST',
+        })
     
+        const res = await tokens.json()
+        .then((access_json) => {
+            this.access_token = access_json.access_token,
+            this.token_type = access_json.token_type,
+            this.expires_in = access_json.expires_in,
+            this.scope = access_json.scope,
+            this.jti = access_json.jti
+        });        
+    }
+
+    async createTableDeliverypoints() {
+        return db.query(`
+          CREATE TABLE _deliverypoints_sdek (
+            id SERIAL PRIMARY KEY,
+            name text,
+            uuid text,
+            work_time text,
+            email text,
+            country_code text,
+            region_code text,
+            region text,
+            city_code text
+            city text
+            postal_code text
+            address text
+          )
+        `)
+      }
+
+    async createTableCities() {
+        return db.query(`
+          CREATE TABLE _cities_sdek (
+            id SERIAL PRIMARY KEY,
+            code integer,
+            city_uuid text,
+            city text,
+            kladr_code text,
+            country_code text,
+            region text,
+            region_code text,
+          )
+        `)
+    }
+
 
 }
--- tests/index.js
+++ tests/index.js
@@ -3,63 +3,131 @@ const grantType = 'client_credentials'
 const clientId = 'EMscd6r9JnFiQ3bLoyjJY6eM78JrJceI'
 const clientSecret = 'PjLZkKBHEiLK3YsjtNrt3TGNG0ahs3kG'
 
+// ответ по офисам полный
+// {
+//     "code": "CHEL11",
+//     "name": "На Пушкина",
+//     "uuid": "3e10e9e9-2232-459a-a809-4c41e366f99d",
+//     "address_comment": "200м от ТРК Куба в сторону ул. Свободы, 60м направо от пересечения ул. К. Маркса и ул. Пушкина.",
+//     "nearest_station": "ул. Цвиллинга",
+//     "work_time": "Пн-Пт 10:00-20:00, Сб-Вс 10:00-16:00",
+//     "phones": [
+//         {
+//             "number": "+79507333305"
+//         }
+//     ],
+//     "email": "o.astahova@cdek.ru",
+//     "note": "200м от ТРК Куба в сторону ул. Свободы, 60м направо от пересечения ул. К. Маркса и ул. Пушкина.",
+//     "type": "PVZ",
+//     "owner_code": "CDEK",
+//     "take_only": false,
+//     "is_handout": true,
+//     "is_reception": true,
+//     "is_dressing_room": true,
+//     "is_ltl": false,
+//     "have_cashless": true,
+//     "have_cash": true,
+//     "allowed_cod": true,
+//     "office_image_list": [
+//         {
+//             "url": "edu.api-pvz.imageRepository.service.cdek.tech:8008/images/1321/88_1_CHEL11"
+//         },
+//         {
+//             "url": "edu.api-pvz.imageRepository.service.cdek.tech:8008/images/1321/90_3_CHEL11"
+//         },
+//         {
+//             "url": "edu.api-pvz.imageRepository.service.cdek.tech:8008/images/1321/89_2_CHEL11"
+//         }
+//     ],
+//     "work_time_list": [
+//         {
+//             "day": 1,
+//             "time": "10:00/20:00"
+//         },
+//         {
+//             "day": 2,
+//             "time": "10:00/20:00"
+//         },
+//         {
+//             "day": 3,
+//             "time": "10:00/20:00"
+//         },
+//         {
+//             "day": 4,
+//             "time": "10:00/20:00"
+//         },
+//         {
+//             "day": 5,
+//             "time": "10:00/20:00"
+//         },
+//         {
+//             "day": 6,
+//             "time": "10:00/16:00"
+//         },
+//         {
+//             "day": 7,
+//             "time": "10:00/16:00"
+//         }
+//     ],
+//     "weight_min": 0.0,
+//     "weight_max": 30.0,
+//     "location": {
+//         "country_code": "RU",
+//         "region_code": 3,
+//         "region": "Челябинская область",
+//         "city_code": 259,
+//         "city": "Челябинск",
+//         "postal_code": "454091",
+//         "longitude": 61.409184,
+//         "latitude": 55.164833,
+//         "address": "Ул. Пушкина, 25",
+//         "address_full": "454091, Россия, Челябинская область, Челябинск, Ул. Пушкина, 25"
+//     },
+//     "fulfillment": false
+// },
 
-class TokenJson {
+// Ответ по Городам
 
-    constructor() {
-        this.access_token = null,
-        this.token_type = null,
-        this.expires_in = null,
-        this.scope = null,
-        this.jti = null
-    }
+// [{
+//     "code": 259,
+//     "city_uuid": "856de44b-c8e4-4890-898d-50088dc00656",
+//     "city": "Челябинск",
+//     "kladr_code": "7400000100000",
+//     "country_code": "RU",
+//     "country": "Россия",
+//     "region": "Челябинская область",
+//     "region_code": 3,
+//     "sub_region": "Челябинский городской округ",
+//     "longitude": 61.4026,
+//     "latitude": 55.16,
+//     "time_zone": "Asia/Yekaterinburg",
+//     "payment_limit": -1.0
+// },]
 
-    get_access_token() {
-        return this.access_token
-    }
+// То что записываю в базу по офисам
 
-    get_jti() {
-        return this.jti
-    }
+// {
+//     "name": "На Пушкина",
+//     "uuid": "3e10e9e9-2232-459a-a809-4c41e366f99d",
+//     "work_time": "Пн-Пт 10:00-20:00, Сб-Вс 10:00-16:00",
+//     "email": "o.astahova@cdek.ru",
+//     "country_code": "RU",
+//     "region_code": 3,
+//     "region": "Челябинская область",
+//     "city_code": 259,
+//     "city": "Челябинск",
+//     "postal_code": "454091",
+//     "address": "Ул. Пушкина, 25",
+// },
 
-    set_access_json(access_json) {
-        this.access_token = access_json.access_token,
-        this.token_type = access_json.token_type,
-        this.expires_in = access_json.expires_in,
-        this.scope = access_json.scope,
-        this.jti = access_json.jti
-    }
+// То что записываю по городам в базу
 
-}
-
-const tokenJson = new TokenJson();
-
-
-// Получение токенов
-async function getTokens(url_get_token, client_credentials, client_id, client_secret) {
-    const tokens = await fetch(`${url_get_token}&grant_type=${client_credentials}&client_id=${client_id}&client_secret=${client_secret}`, {
-        method: 'POST',
-    })
-
-    const res = await tokens.json()
-    .then((r) => {
-        console.log(r)
-        tokenJson.set_access_json(r)
-    });    
-}
-
-// Получение списка офисов
-async function getDeliverypoints() {
-    const deliv = await fetch(`https://api.edu.cdek.ru/v2/deliverypoints?size=1`, {
-        headers: {
-            Authorization: `Bearer ${tokenJson.access_token}`
-        }
-    });
-
-    const res = await deliv.json();
-    console.log(res);
-}
-
-getTokens(urlGetToken, grantType, clientId, clientSecret);
-console.log(tokenJson.get_access_token());
-// getDeliverypoints();
+// {
+    //     "code": 259,
+    //     "city_uuid": "856de44b-c8e4-4890-898d-50088dc00656",
+    //     "city": "Челябинск",
+    //     "kladr_code": "7400000100000",
+    //     "country_code": "RU",
+    //     "region": "Челябинская область",
+    //     "region_code": 3,
+    // }
